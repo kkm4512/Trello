83e2c4edff4d37ba843916c624d0e3c1081b0037
tae98 <98rlaxogus@gmail.com>
2024-10-14 20:52:37 +0900

    회원가입 init timestamped추가

--- src/main/java/com/example/trello/domain/user/entity/User.java
+++ src/main/java/com/example/trello/domain/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.example.trello.domain.user.entity;
 
+import com.example.trello.common.entity.Timestamped;
 import com.example.trello.domain.user.dto.UserRequestDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -7,15 +8,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SoftDelete;
 
-import java.math.BigInteger;
-
 @Entity
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @SoftDelete(columnName = "deleted")
 @Table(name ="users")
-public class User extends TimeStamped{
+public class User extends Timestamped {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
