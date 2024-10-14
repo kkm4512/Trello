921b81acb7f93ce7224f499ee35d7987d884a34c
kkm4512 <nayoun440@naver.com>
2024-10-14 17:13:59 +0900

    init

---
+++ src/main/java/com/example/trello/TrelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trello/TrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
