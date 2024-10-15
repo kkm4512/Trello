6f06213dea8c0fcfb9ae396dca42af2eadacd8ca
kkm4512 <nayoun440@naver.com>
2024-10-15 10:56:05 +0900

    feat. Slack 알림 관련 기초 세팅

---
+++ src/test/java/com/example/trello/domain/slack/SlackServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.trello.domain.slack;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.convert.DurationFormat;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+class SlackServiceTest {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    SlackService slackService;
+
+    @Test
+    @DisplayName("Slack AOP Test")
+    void test1(){
+        slackService.SlackTest();
+    }
+
+}
