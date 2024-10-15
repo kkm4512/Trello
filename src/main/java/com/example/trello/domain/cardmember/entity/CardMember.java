29f2f8891f1d6050408e5a85a59faedddcfdf0aa
kkm4512 <nayoun440@naver.com>
2024-10-16 08:00:14 +0900

    refactor. CD 기능 수정 v7 1. @SoftDelete로 인한 User칼럼을 Eager로 변경

--- src/main/java/com/example/trello/domain/cardmember/entity/CardMember.java
+++ src/main/java/com/example/trello/domain/cardmember/entity/CardMember.java
@@ -16,7 +16,7 @@ public class CardMember extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User user;
 
