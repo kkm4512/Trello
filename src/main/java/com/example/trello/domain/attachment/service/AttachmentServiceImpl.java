9d2e998220486a004547e987b990f8e518dccaa8
kkm4512 <nayoun440@naver.com>
2024-10-15 20:28:11 +0900

    refactor. 파일 관련 기능 분할

--- src/main/java/com/example/trello/domain/attachment/service/AttachmentServiceImpl.java
+++ src/main/java/com/example/trello/domain/attachment/service/AttachmentServiceImpl.java
@@ -7,103 +7,106 @@ import com.example.trello.common.response.ApiResponseFileEnum;
 import com.example.trello.common.response.ApiResponseUserEnum;
 import com.example.trello.domain.attachment.entity.Attachment;
 import com.example.trello.domain.attachment.repository.AttachmentRepository;
+import com.example.trello.domain.card.repository.CardRepository;
 import com.example.trello.domain.user.dto.AuthUser;
 import com.example.trello.domain.user.entity.User;
 import com.example.trello.domain.user.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import jdk.jshell.spi.ExecutionControl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-import static com.example.trello.common.path.GlobalPath.*;
+import static com.example.trello.common.path.GlobalPath.BASE_URL;
+import static com.example.trello.common.path.GlobalPath.SEPARATOR;
+import static com.example.trello.common.response.ApiResponseFileEnum.*;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AttachmentServiceImpl implements AttachmentService {
+    // Attachment 기능 관련 서비스
+    private final AttachmentPathService pathService;
+    private final AttachmentFileService fileService;
+    private final AttachmentDirectoryService directoryService;
+
+    // 그 외
     private final AttachmentRepository attachmentRepository;
+    private final CardRepository cardRepository;
     private final UserRepository userRepository;
 
     @Override
     @Transactional
     public ApiResponse<List<String>> uploads(AuthUser authUser, String card_id, List<MultipartFile> files) {
+        List<String> fileNames = new ArrayList<>();
         try {
             User user = userRepository.findById(authUser.getId()).orElseThrow(() -> new UserException(ApiResponseUserEnum.USER_NOT_FOUND));
-            Path directory = Paths.get("files")
-                    .resolve("cards")
-                    .resolve(card_id);
-            Files.createDirectories(directory);
-            List<String> paths = new ArrayList<>();
+            //TODO: Card Exception + cardRepositroy에서 찾는것으로 변경 해야함
+//            Card card = cardRepository.findById(Long.parseLong(card_id)).orElseThrow(() -> new UserException(ApiResponseUserEnum.USER_NOT_FOUND));
+            Path directory = pathService.mkFilesCardsPath(card_id);
+            directoryService.mkdir(directory);
             for ( MultipartFile file : files ) {
                 // 파일이 존재할때만
                 if (!(file == null || file.isEmpty())) {
-                    String filename = UUID.randomUUID() + DELIMITER + file.getOriginalFilename();
-                    Path filePath = Paths.get(directory.toString(),filename);
+                    String filename = fileService.mkFilename(file.getOriginalFilename());
+                    Path filePath = pathService.mkPath(directory, filename);
                     // DB저장
                     Attachment attachment = Attachment.builder()
                             .path(filePath.toString())
+                            .originFileName(file.getOriginalFilename())
                             .user(user)
+                            //TODO: Card로 바꿔야함
+                            .card_id(Long.parseLong(card_id))
                             .build();
                     attachmentRepository.save(attachment);
                     // inputStream의 자원 누수를 방지하기위한 try - with - resources
-                    try (InputStream inputStream = file.getInputStream()){
-                        Files.copy(inputStream, filePath);
-                        paths.add(filePath.toString());
-                    } catch (IOException e){
-                        log.error(e.getMessage(),e);
-                        throw new FileException(ApiResponseFileEnum.FILE_IO_ERROR);
-                    }
+                    fileService.mkFile(filePath, file);
+                    fileNames.add(file.getOriginalFilename());
                 }
             }
-            return new ApiResponse<>(ApiResponseFileEnum.FILE_OK,paths);
+            return new ApiResponse<>(ApiResponseFileEnum.FILE_OK,fileNames);
         } catch (Exception e) {
             log.error(e.getMessage(),e);
-            throw new FileException(ApiResponseFileEnum.FILE_IO_ERROR);
+            throw new FileException(FILE_IO_ERROR);
         }
     }
 
     @Override
     public ApiResponse<List<String>> downloads(String card_id) {
+        List<String> data = new ArrayList<>();
         try {
-            Path path = Paths.get("files")
-                    .resolve("cards")
-                    .resolve(card_id);
-            List<String> data = Files.list(path)
+            Path path = pathService.mkFilesCardsPath(card_id);
+             data = Files.list(path)
                     .map(p -> BASE_URL +
                             SEPARATOR + path.toString() +
                             SEPARATOR + p.getFileName().toString())
                     .toList();
-            return ApiResponse.of(ApiResponseFileEnum.FILE_OK,data);
+            return ApiResponse.of(FILE_OK,data);
         } catch (Exception e) {
+            if (data.isEmpty()) {
+                return ApiResponse.of(FILE_OK,data);
+            }
             log.error(e.getMessage(),e);
-            throw new FileException(ApiResponseFileEnum.FILE_NOT_FOUND);
+            throw new FileException(FILE_NOT_FOUND);
         }
     }
 
     @Override
     public ApiResponse<List<String>> deletes(String card_id) {
         try {
-            Path path = Paths.get("files")
-                    .resolve("cards")
-                    .resolve(card_id);
+            Path path = pathService.mkFilesCardsPath(card_id);
             List<String> data = Files.list(path)
                     .peek(this::delete)
                     .map(String::valueOf)
                     .toList();
-            return ApiResponse.of(ApiResponseFileEnum.FILE_OK,data);
+            return ApiResponse.of(FILE_OK,data);
         } catch (Exception e){
-            throw new FileException(ApiResponseFileEnum.FILE_NOT_FOUND);
+            throw new FileException(FILE_NOT_FOUND);
         }
 
     }
@@ -112,7 +115,7 @@ public class AttachmentServiceImpl implements AttachmentService {
         try {
             Files.delete(path);
         } catch (Exception e) {
-            throw new FileException(ApiResponseFileEnum.FILE_NOT_FOUND);
+            throw new FileException(FILE_NOT_FOUND);
         }
 
     }
