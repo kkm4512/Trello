c24dc1a236811ee31222a8fb8512e0517f9e441f
ican0422 <ican0422@gmail.com>
2024-10-16 18:57:39 +0900

    [refactor] : 카드 리펙토링

--- src/main/java/com/example/trello/domain/card/service/CardService.java
+++ src/main/java/com/example/trello/domain/card/service/CardService.java
@@ -49,26 +49,17 @@ public class CardService {
     /* 카드 등록 */
     @Transactional
     public ApiResponse<SaveCardResponse> saveCard(AuthUser authUser, Long workspaceId, Long boardsId, Long listId, SaveCardRequest request) {
-        // 워크 스페이스, 보더, 리스트 존재하는지 확인
-        boolean isWorkspace = workspaceRepository.existsById(workspaceId);
-        boolean isBoard = boardRepository.existsById(boardsId);
-        if (!isWorkspace) {
-            throw new IllegalArgumentException("해당 워크 스페이스가 없습니다.");
-        }
-        if (!isBoard) {
-            throw new IllegalArgumentException("해당 보더가 없습니다.");
-        }
-        BoardList list = listRepository.findById(listId).orElseThrow(
-                () -> new IllegalArgumentException("해당 리스트가 없습니다."));
+        // 워크 스페이스, 보더 존재하는지 확인
+        validateWorkspaceAndBoard(workspaceId, boardsId);
 
-        // 보더에 등록된 멤버인지 확인
-        Member user = memberRepository.findByUserId(authUser.getId()).orElseThrow(
-                () -> new IllegalArgumentException("해당 유저는 멤버가 아닙니다."));
+        // 리스트 가져오기
+        BoardList list = listOrElseThrow(listId);
+
+        // 로그인한 유저가 워크스페이스 멤버 등록 유저인지 확인
+        Member user = memberOrElseThrow(authUser);
 
         // 읽기 권한인지 확인
-        if (user.getMemberRole() == MemberRole.READ_ONLY) {
-            throw new IllegalArgumentException("권한이 없습니다.");
-        }
+        userMemberRole(user);
 
         // 카드 등록
         Card card = cardRepository.save(new Card(request, list, user));
@@ -82,33 +73,17 @@ public class CardService {
     @CardChangeSlack
     @Transactional
     public ApiResponse<PutCardResponse> updateCard(AuthUser authUser, Long workspaceId, Long boardsId, Long listId, Long cardId, PutCardRequest request) {
-        // 보더에 등록된 멤버인지 확인
-        Member user = memberRepository.findByUserId(authUser.getId()).orElseThrow(
-                () -> new IllegalArgumentException("해당 유저는 멤버가 아닙니다."));
+        // 로그인한 유저가 워크스페이스 멤버 등록 유저인지 확인
+        Member user = memberOrElseThrow(authUser);
 
         // 읽기 권한인지 확인
-        if (user.getMemberRole() == MemberRole.READ_ONLY) {
-            throw new IllegalArgumentException("권한이 없습니다.");
-        }
+        userMemberRole(user);
 
-        boolean isWorkspace = workspaceRepository.existsById(workspaceId);
-        boolean isBoard = boardRepository.existsById(boardsId);
-        boolean list = listRepository.existsById(listId);
-        boolean cardMember = cardMemberRepository.existsByUserIdAndCardId(user.getUser().getId(), cardId);
-        if (!isWorkspace) {
-            throw new IllegalArgumentException("해당 워크 스페이스가 없습니다.");
-        }
-        if (!isBoard) {
-            throw new IllegalArgumentException("해당 보더가 없습니다.");
-        }
-        if (!list) {
-            throw new IllegalArgumentException("해당 리스트가 없습니다.");
-        }
-        if (!cardMember) {
-            throw new IllegalArgumentException("해당 카드 담당자가 아닙니다.");
-        }
-        Card card = cardRepository.findById(cardId).orElseThrow(
-                () -> new IllegalArgumentException("해당 카드가 없습니다."));
+        // 워크스페이스, 보더, 리스트, 카드 담당자 유무 올인원
+        check_All_In_One(workspaceId, boardsId, listId, cardId, user);
+
+        // 카드 가져오기
+        Card card = cardOrElseThrow(cardId);
 
         card.update(request);
 
@@ -119,25 +94,14 @@ public class CardService {
 
     /* 카드 상세 조회 */
     public ApiResponse<GetCardResponse> getCard(AuthUser authUser, Long workspaceId, Long boardsId, Long listId, Long cardId) {
-        // 보더에 등록된 멤버인지 확인
-        Member user = memberRepository.findByUserId(authUser.getId()).orElseThrow(
-                () -> new IllegalArgumentException("해당 유저는 멤버가 아닙니다."));
+        // 로그인한 유저가 워크스페이스 멤버 등록 유저인지 확인
+        Member user = memberOrElseThrow(authUser);
 
-        boolean isWorkspace = workspaceRepository.existsById(workspaceId);
-        boolean isBoard = boardRepository.existsById(boardsId);
-        boolean list = listRepository.existsById(listId);
-        if (!isWorkspace) {
-            throw new IllegalArgumentException("해당 워크 스페이스가 없습니다.");
-        }
-        if (!isBoard) {
-            throw new IllegalArgumentException("해당 보더가 없습니다.");
-        }
-        if (!list) {
-            throw new IllegalArgumentException("해당 리스트가 없습니다.");
-        }
+        // 워크스페이스, 보더, 리스트 유무 확인
+        validateWorkspaceAndBoardAndList(workspaceId, boardsId, listId);
 
-        Card card = cardRepository.findById(cardId).orElseThrow(
-                () -> new IllegalArgumentException("해당 카드가 없습니다."));
+        // 카드 가져오기
+        Card card = cardOrElseThrow(cardId);
 
         List<Comment> comments = commentRepository.findByCardId(card.getId());
         List<CardCommentInfo> commentInfos = comments.stream().map(CardCommentInfo::new).toList();
@@ -155,10 +119,88 @@ public class CardService {
                 () -> new IllegalArgumentException("해당 유저는 멤버가 아닙니다."));
 
         // 읽기 권한인지 확인
+        userMemberRole(user);
+
+        // 워크스페이스, 보더, 리스트, 카드 담당자 유무 올인원
+        check_All_In_One(workspaceId, boardsId, listId, cardId, user);
+
+        // 카드 가져오기
+        Card card = cardOrElseThrow(cardId);
+
+        cardRepository.delete(card);
+
+        ApiResponseEnum apiResponseEnum = ApiResponseCardEnum.CARD_SEARCH_OK;
+        ApiResponse<deleteResponse> apiResponse = new ApiResponse<>(apiResponseEnum, new deleteResponse(card));
+        return apiResponse;
+    }
+
+    /* 카드 검색 */
+    public ApiResponse<Page<SearchCardResponse>> searchCard(Long workspaceId, Long boardsId, SearchCardRequest request, int page, int size) {
+        validateWorkspaceAndBoard(workspaceId, boardsId);
+
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<SearchCardResponse> cards = cardRepository.searchCards(request, pageable);
+
+        ApiResponseEnum apiResponseEnum = ApiResponseCardEnum.CARD_SEARCH_OK;
+        return new ApiResponse<>(apiResponseEnum, cards);
+    }
+
+    /* 유저 읽기 권한 확인 */
+    private void userMemberRole(Member user) {
         if (user.getMemberRole() == MemberRole.READ_ONLY) {
             throw new IllegalArgumentException("권한이 없습니다.");
         }
+    }
 
+    /* 로그인한 유저 정보 */
+    private Member memberOrElseThrow(AuthUser authUser) {
+        return memberRepository.findByUserId(authUser.getId()).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저는 멤버가 아닙니다."));
+    }
+
+    /* 리스트 확인 */
+    private BoardList listOrElseThrow(Long listId) {
+        return listRepository.findById(listId).orElseThrow(
+                () -> new IllegalArgumentException("해당 리스트가 없습니다."));
+    }
+
+    /* 카드 가져오기 */
+    private Card cardOrElseThrow(Long cardId) {
+        return cardRepository.findById(cardId).orElseThrow(
+                () -> new IllegalArgumentException("해당 카드가 없습니다."));
+    }
+
+    /* 워크스페이스, 보더 유무 확인 */
+    private void validateWorkspaceAndBoard(Long workspaceId, Long boardsId) {
+        // 워크 스페이스, 보더, 리스트 존재하는지 확인
+        boolean isWorkspace = workspaceRepository.existsById(workspaceId);
+        boolean isBoard = boardRepository.existsById(boardsId);
+        if (!isWorkspace) {
+            throw new IllegalArgumentException("해당 워크 스페이스가 없습니다.");
+        }
+        if (!isBoard) {
+            throw new IllegalArgumentException("해당 보더가 없습니다.");
+        }
+    }
+
+    /* 워크 스페이스, 보더, 리스트 유무 확인 */
+    private void validateWorkspaceAndBoardAndList(Long workspaceId, Long boardsId, Long listId) {
+        boolean isWorkspace = workspaceRepository.existsById(workspaceId);
+        boolean isBoard = boardRepository.existsById(boardsId);
+        boolean list = listRepository.existsById(listId);
+        if (!isWorkspace) {
+            throw new IllegalArgumentException("해당 워크 스페이스가 없습니다.");
+        }
+        if (!isBoard) {
+            throw new IllegalArgumentException("해당 보더가 없습니다.");
+        }
+        if (!list) {
+            throw new IllegalArgumentException("해당 리스트가 없습니다.");
+        }
+    }
+
+    /* 워크스페이스, 보더, 리스트, 카드 담당자 유무 올인원 */
+    private void check_All_In_One(Long workspaceId, Long boardsId, Long listId, Long cardId, Member user) {
         boolean isWorkspace = workspaceRepository.existsById(workspaceId);
         boolean isBoard = boardRepository.existsById(boardsId);
         boolean list = listRepository.existsById(listId);
@@ -175,31 +217,5 @@ public class CardService {
         if (!cardMember) {
             throw new IllegalArgumentException("해당 카드 담당자가 아닙니다.");
         }
-        Card card = cardRepository.findById(cardId).orElseThrow(
-                () -> new IllegalArgumentException("해당 카드가 없습니다."));
-
-        cardRepository.delete(card);
-
-        ApiResponseEnum apiResponseEnum = ApiResponseCardEnum.CARD_SEARCH_OK;
-        ApiResponse<deleteResponse> apiResponse = new ApiResponse<>(apiResponseEnum, new deleteResponse(card));
-        return apiResponse;
-    }
-
-
-    public ApiResponse<Page<SearchCardResponse>> searchCard(Long workspaceId, Long boardsId, SearchCardRequest request, int page, int size) {
-        boolean isWorkspace = workspaceRepository.existsById(workspaceId);
-        boolean isBoard = boardRepository.existsById(boardsId);
-        if (!isWorkspace) {
-            throw new IllegalArgumentException("해당 워크 스페이스가 없습니다.");
-        }
-        if (!isBoard) {
-            throw new IllegalArgumentException("해당 보더가 없습니다.");
-        }
-
-        Pageable pageable = PageRequest.of(page - 1, size);
-        Page<SearchCardResponse> cards = cardRepository.searchCards(request, pageable);
-
-        ApiResponseEnum apiResponseEnum = ApiResponseCardEnum.CARD_SEARCH_OK;
-        return new ApiResponse<>(apiResponseEnum, cards);
     }
 }
