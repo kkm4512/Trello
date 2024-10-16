161c3a1f8ad0ab7689f9d857cd85211e0019bf89
ican0422 <ican0422@gmail.com>
2024-10-16 22:29:55 +0900

    [update] : 카드 상세 조회 시 카드 담당자 출력

--- src/main/java/com/example/trello/domain/card/dto/response/GetCardResponse.java
+++ src/main/java/com/example/trello/domain/card/dto/response/GetCardResponse.java
@@ -1,6 +1,7 @@
 package com.example.trello.domain.card.dto.response;
 
 import com.example.trello.domain.card.entity.Card;
+import com.example.trello.domain.cardmember.dto.MemberInfo;
 import com.example.trello.domain.comment.dto.CardCommentInfo;
 import com.example.trello.domain.comment.entity.Comment;
 import lombok.Getter;
@@ -12,21 +13,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
-//@NoArgsConstructor
 public class GetCardResponse {
     private final Long id;
     private final String title;
     private final String content;
     private final LocalDateTime createdAt;
     private final LocalDateTime updatedAt;
+    private final List<MemberInfo> members;
     private final List<CardCommentInfo> comments;
 
-    public GetCardResponse(Card card, List<CardCommentInfo> comments) {
+    public GetCardResponse(Card card,List<MemberInfo> members, List<CardCommentInfo> comments) {
         this.id = card.getId();
         this.title = card.getTitle();
         this.content = card.getContent();
         this.createdAt = card.getCreatedAt();
         this.updatedAt = card.getUpdatedAt();
+        this.members = members;
         this.comments = comments;
     }
 }
--- src/main/java/com/example/trello/domain/cardmember/repository/CardMemberRepository.java
+++ src/main/java/com/example/trello/domain/cardmember/repository/CardMemberRepository.java
@@ -4,7 +4,11 @@ import com.example.trello.domain.cardmember.entity.CardMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CardMemberRepository extends JpaRepository<CardMember, Long> {
     boolean existsByUserIdAndCardId(Long userId, Long cardId);
+
+    List<CardMember> findByCardId(Long id);
 }
